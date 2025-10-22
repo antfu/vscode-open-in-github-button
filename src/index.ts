1cc0de7aad936d0abbe08c0eb8b8e00058e11317
jinghaihan <jhh19980114@gmail.com>
2025-10-22 18:04:35 +0800

    feat: add align priority configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { StatusBarAlignment, window } from 'vscode'
+import { StatusBarAlignment, window, workspace } from 'vscode'
+
+function alignPriority(): number {
+  return +(workspace.getConfiguration('open-in-github-button').get('alignPriority') as string)
+}
 
 export function activate() {
-  const statusBar = window.createStatusBarItem(StatusBarAlignment.Left, 0)
+  const statusBar = window.createStatusBarItem(StatusBarAlignment.Left, alignPriority())
   statusBar.command = 'openInGitHub.openProject'
   statusBar.text = '$(github)'
   statusBar.tooltip = 'Open in GitHub'
